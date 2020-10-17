9f043716f428a8ab668b79b595b8b284d773986c
Anwesh43 <anweshthecool0@gmail.com>
2020-10-17 16:52:49 +0530

    adding g fixes

---
+++ index.ts
@@ -0,0 +1,14 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 3 
+const strokeFactor : number = 90 
+const sizeFactor : number = 9.9 
+const backColor : string = "#BDBDBD"
+const delay : number = 20 
+const colors : Array<string> = [
+    "#F44336",
+    "#3F51B5",
+    "#2196F3",
+    "#009688",
+    "#FF9800"
+]
